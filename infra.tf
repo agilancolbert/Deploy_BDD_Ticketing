2f95c99599277335a4e9b5f1790c372fe0fe19f0
agilancolbert <35196337+agilancolbert@users.noreply.github.com>
2020-07-06 21:31:18 +0200

    Create infra.tf

---
+++ infra.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+# Test Base RDS
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "admin"
+  password             = "azerty1234"
+  parameter_group_name = "default.mysql5.7"
+  publicly_accessible = "true"
+}
